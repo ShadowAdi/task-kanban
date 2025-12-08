9feee7af763aaa0c59243b8f871db06f34f94d3d
ShadowAdi <shadowshukla76@gmail.com>
2025-12-09 01:46:11 +0530

    added task popover

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@ import { Plus } from "lucide-react";
 
 export default function Home() {
   return (
-    <main className="flex flex-col w-full min-h-screen px-6 py-8">
+    <main className="flex flex-col w-full min-h-screen px-8 py-8">
       <div className="flex items-center w-full justify-between ">
-        <h1 className="text-3xl font-medium">
+        <h1 className="text-4xl font-medium">
           All Taks
         </h1>
-        <Button className="text-base ">
+        <Button className="text-base cursor-pointer!">
           <Plus />
           Create New
         </Button>
