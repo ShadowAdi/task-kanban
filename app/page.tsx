c245ccc19314e39ec2aa018ce972eac6d441cfe2
ShadowAdi <shadowshukla76@gmail.com>
2025-12-09 01:37:35 +0530

    added shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <main className="flex flex-col w-full min-h-screen px-6 py-8">
       <div className="flex items-center w-full justify-between ">
-        <h1 className="text-xl font-semibold">
+        <h1 className="text-3xl font-medium">
           All Taks
         </h1>
 
